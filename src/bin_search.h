1aba927379d41e83f3efcb6238d9b33b59b3f30a
Daniel Skýpala <daniel@honza.info>
2024-07-11 16:13:28 +0200

    Binary search

---
+++ src/bin_search.h
@@ -0,0 +1,25 @@
+#include <functional>
+
+using namespace std;
+    
+template<typename T>
+T binary_search(const function<bool(T)>& predicate, T lower, T upper) {
+    while (lower + 1 < upper) {
+        T mid = (lower + upper) / 2;
+        if (predicate(mid)) {
+            upper = mid;
+        } else {
+            lower = mid;
+        }
+    }
+    return lower + 1;
+}
+
+template<typename T>
+T binary_search(const function<bool(T)>& predicate, T lower) {
+    T diff = 1;
+    while (!predicate(lower + diff)) {
+        diff *= 2;
+    }
+    return binary_search(predicate, lower + diff/2, lower + diff);
+}
