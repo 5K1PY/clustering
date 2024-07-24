fdd6fdb8b25e62179e7b3453bdce906278492cee
Daniel Skýpala <daniel@honza.info>
2024-07-24 10:04:54 +0200

    SolutionCost: Add solution cost calculation

---
+++ src/solution_cost.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <limits>
+
+#include "points.h"
+
+using namespace std;
+
+double f_param = 1.0;
+
+int main(int argc, char const *argv[]) {
+    ifstream solution("solution.txt");
+    int n, dim; cin >> n >> dim;
+    auto points = load_points(n, dim);
+
+    int x = 0;
+    vector<int> facilities;
+    while (solution >> x) {
+        facilities.push_back(x);
+    }
+
+    double cost = facilities.size() * f_param;
+    for (auto point: points) {
+        double min_dist = numeric_limits<double>::infinity();
+        for (auto facility_i: facilities) {
+            min_dist = min(min_dist, point.dist(points[facility_i]));
+        }
+        cost += min_dist;
+    }
+
+    cout << cost << endl;
+}
