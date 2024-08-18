741179fe9792fe42622c66e3c17c5637a3779ebc
Daniel Skýpala <daniel@honza.info>
2024-08-19 00:25:00 +0200

    Test: Run on different arguments

--- test.py
+++ test.py
@@ -6,10 +6,18 @@ BUILD_DIR = "build"
 DATA_DIR = "data"
 GENERATOR = "data_gen"
 JUDGE = "solution_cost"
-SOLUTIONS = ["mettu_plaxton", "facility_set"]
+SOLUTIONS = ["mettu_plaxton"] + ["facility_set"]*5
+SOLUTION_ARGS = [
+    "",
+    "3c6da5d7",
+    "5b17b966",
+    "9a7aa40",
+    "3c6da5d7",
+    "8efbc170",
+]
 
-SIZES = [50]
-DIMENSIONS = [2, 10]
+SIZES = [10, 50, 500, 5000]
+DIMENSIONS = [2, 3]
 COST = 1
 
 def gen(size, dimension, cost) -> str:
@@ -26,10 +34,10 @@ def gen(size, dimension, cost) -> str:
     return filepath
 
 
-def solve(input_path, solution) -> str:
+def solve(input_path, solution, args) -> str:
     output_path = input_path.removesuffix(".in") + f".{solution}.out"
     process = Popen(
-        [os.path.join(BUILD_DIR, solution)],
+        [os.path.join(BUILD_DIR, solution), *args],
         stdin=open(input_path),
         stdout=open(output_path, "w")
     )
@@ -54,7 +62,8 @@ os.makedirs(DATA_DIR, exist_ok=True)
 for size in SIZES:
     for dimension in DIMENSIONS:
         inp = gen(size, dimension, COST)
-        for solution in SOLUTIONS:
-            print(f"{inp:<20} {solution:15}", end=" ")
-            out = solve(inp, solution)
+        for solution, args in zip(SOLUTIONS, SOLUTION_ARGS, strict=True):
+            print(f"{inp:<20} {solution:15}", end=" ", flush=True)
+            out = solve(inp, solution, args)
             print(judge(inp, out))
+        print("-"*50)
