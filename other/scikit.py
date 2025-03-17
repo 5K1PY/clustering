82f619ebb89dd22ad5bed5aeeebf06dc5dbf448f
Daniel Skýpala <daniel@honza.info>
2025-03-17 12:24:28 +0100

    Add Scikit k-means solution

---
+++ other/scikit.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.cluster import KMeans
+
+def main():
+    num_points, dimension, k = map(int, input().split())
+
+    points = [
+        list(map(float, input().split()))
+        for _ in range(num_points)
+    ]
+
+    data = np.array(points)
+
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(data)
+
+    print(kmeans.inertia_)
+
+
+if __name__ == "__main__":
+    main()
