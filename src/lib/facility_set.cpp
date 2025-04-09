ac603f890ff10af07471a34d247a9803e8bd5057
Daniel Skýpala <daniel@honza.info>
2025-04-09 10:32:11 +0200

    FacilitySet: Fix beta and tau parameters

--- src/lib/facility_set.cpp
+++ src/lib/facility_set.cpp
@@ -8,6 +8,8 @@
 #include "facility_set.hpp"
 #include "pow_z.hpp"
 
+const double beta_mul = 0.25;
+
 std::vector<int> compute_facilities(int dim, std::vector<tagged_point> points, double facility_cost, HashingScheme hashing_scheme) {
     for (auto &p: points) {
         p.label = randRange(0ULL, std::numeric_limits<ull>::max());
@@ -17,16 +19,18 @@ std::vector<int> compute_facilities(int dim, std::vector<tagged_point> points, d
     std::vector<const tagged_point*> min_labels(points.size(), NULL);
 
     double r_guess = 1.0 / scale;
+    double beta = beta_mul * 3.0 * get_gamma(hashing_scheme, dim);
+    double tau = pow(beta, 0.25*Z);
     while (find(r_approx.begin(), r_approx.end(), 0) != r_approx.end()) {
         std::vector<int> approx_ball_sizes = eval_composable(dim, points, r_guess, Composable::Size, hashing_scheme);
         std::vector<const tagged_point*> guess_min_labels = eval_composable(dim, points, r_guess, Composable::MinLabel, hashing_scheme);
         for (int i=0; i<(int) points.size(); i++) {
             if (r_approx[i] != 0) continue;
-            if (approx_ball_sizes[i] >= facility_cost / (2 * POWZ(beta_param) * POWZ(r_guess))) {
+            if (approx_ball_sizes[i] >= facility_cost / (2 * POWZ(beta) * POWZ(r_guess))) {
                 r_approx[i] = r_guess;
                 min_labels[i] = guess_min_labels[i];
             } else if (approx_ball_sizes[i] == (int) points.size()) {
-                r_approx[i] = INVPOWZ(facility_cost / (2 * POWZ(beta_param) * points.size()));
+                r_approx[i] = INVPOWZ(facility_cost / (2 * POWZ(beta) * points.size()));
                 min_labels[i] = guess_min_labels[i];
             }
         }
@@ -35,7 +39,7 @@ std::vector<int> compute_facilities(int dim, std::vector<tagged_point> points, d
 
     std::vector<int> results;
     for (int i=0; i<(int) points.size(); i++) {
-        if (&points[i] == min_labels[i] || randBool(tau_param * POWZ(r_approx[i]) / facility_cost))
+        if (&points[i] == min_labels[i] || randBool(POWZ(tau) * POWZ(r_approx[i]) / facility_cost))
             results.push_back(i);
     }
     return results;
--- src/lib/facility_set.hpp
+++ src/lib/facility_set.hpp
@@ -1,8 +1,3 @@
 #include "hashing.hpp"
 
-const double tau_param = 1.0;
-// TODO: Beta has to satisfy:
-// B_P(p, r) ⊆ A_P^β(p, r) ⊆ BP(p, βr).
-const double beta_param = 1.0;
-
 std::vector<int> compute_facilities(int dim, std::vector<tagged_point> points, double facility_cost, HashingScheme hashing_scheme);
