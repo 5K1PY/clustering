18cede15e0751adb10250d8ff730fda493a8a0bf
Daniel Skýpala <daniel@honza.info>
2025-05-01 17:23:06 +0200

    DataGen: Restrict generated range

--- src/data_gen.cpp
+++ src/data_gen.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
@@ -12,7 +13,16 @@ constexpr ull CLUSTER_RADIUS = 1e15;
 point rand_point(int dim) {
     point p(dim);
     for (int i=0; i<dim; i++) {
-        p[i] = randRange(0ULL, MAX_COORD);
+        p[i] = randRange<ull>(0ULL, MAX_COORD);
+    }
+    return p;
+}
+
+point rand_cluster_center(int dim) {
+    point p(dim);
+    for (int i=0; i<dim; i++) {
+        // Restrict range to generate points within [0, MAX_COORD]
+        p[i] = randRange<ull>(10*CLUSTER_RADIUS, MAX_COORD-10*CLUSTER_RADIUS);
     }
     return p;
 }
@@ -20,7 +30,7 @@ point rand_point(int dim) {
 point rand_shift(int dim) {
     point p(dim);
     for (int i=0; i<dim; i++) {
-        p[i] = randNormal(0ULL, CLUSTER_RADIUS);
+        p[i] = randNormal<ll>(0LL, CLUSTER_RADIUS);
     }
     return p;
 }
@@ -42,7 +52,7 @@ std::vector<point> gen_clusters(int n, int dim) {
 
     std::vector<point> centers(cluster_count, point(dim));
     for (int i=0; i<cluster_count; i++) {
-        centers[i] = all_points[all_points_sz++] = rand_point(dim);
+        centers[i] = all_points[all_points_sz++] = rand_cluster_center(dim);
     }
 
     for (int i=0; i<free_points; i++) {
@@ -69,6 +79,9 @@ int main() {
     std::cout << std::fixed << std::setprecision(10);
     for (auto p: points) {
         for (int i=0; i<dimension; i++) {
+            // Theoretically it's possible to hit outside of this range
+            // but the even hitting enough shift of has chance of Phi(10) which is basically zero
+            assert(0 <= p[i] && p[i] <= (ll) MAX_COORD);
             std::cout << ((double) p[i] / scale) << (i+1 < dimension ? " " : "\n");
         }
     }
