3de58a477553b5e2940ab09bbc51dfbb7a235abf
Daniel Skýpala <daniel@honza.info>
2025-05-03 18:19:50 +0200

    FacilitySet: Add documentation

--- src/lib/facility_set.hpp
+++ src/lib/facility_set.hpp
@@ -1,3 +1,14 @@
 #include "hashing.hpp"
 
+/**
+ * @brief Computes set of facilities to open for some set of points P.
+ *
+ * See https://arxiv.org/pdf/2307.07848 Algorithm 2.
+ *
+ * @param dim The dimension of the space.
+ * @param points The set of points P.
+ * @param facility_cost The cost per one opened facility.
+ * @param hs_choice The choice of hashing scheme to use.
+ * @return Set of facilities as indexes into set of points P.
+ */
 std::vector<int> compute_facilities(int dim, std::vector<tagged_point> points, double facility_cost, HashingSchemeChoice hs_choice);
