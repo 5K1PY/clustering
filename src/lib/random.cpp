07d626223ee2655d790b4c259cd2248636869839
Daniel Skýpala <daniel@honza.info>
2025-05-03 17:47:06 +0200

    Random: Add documentation

--- src/lib/random.cpp
+++ src/lib/random.cpp
@@ -10,24 +10,11 @@ void seed(ull seed) {
 }
 
 double randDouble(double from, double to) {
-    /**
-     * @brief Generates random double from [from, to)
-     * 
-     * @param from lower bound (inclusive)
-     * @param to   upper bound (exclusive)
-     * @return random double from [from, to)
-     */
     std::uniform_real_distribution<double> dist(from, to);
     return dist(rng);
 }
 
 bool randBool(double p = 0.5) {
-    /**
-     * @brief Generates true with probability p
-     * 
-     * @param p probability of true
-     * @return true with probability p, false otherwise 
-     */
     return randDouble(0, 1) <= p;
 }
 
--- src/lib/random.hpp
+++ src/lib/random.hpp
@@ -6,30 +6,45 @@
 
 extern std::mt19937 rng;
 
+/**
+ * @brief Initialize the random number generator.
+ * @param seed The initialization seed
+ */
 void seed(ull seed);
+
+/**
+ * @brief Generates random integer from [min, max] inclusive
+ * @param min The lower bound (inclusive)
+ * @param max The upper bound (inclusive)
+ * @return A random integer between [min, max] inclusive
+ */
 template<typename T> T randRange(T min, T max) {
-    /**
-     * @brief Generates random integer from [min, max] inclusive
-     * 
-     * @param min lower bound (inclusive)
-     * @param max upper bound (inclusive)
-     * @return random integer between [min, max] inclusive
-     */
     std::uniform_int_distribution<T> dist(min, max);
     return dist(rng);
 }
 
+/**
+ * @brief Generates random value from normal distribution N(mean, stddev^2)
+ * @param mean The mean of the normal distribution
+ * @param stddev The standard deviation of the normal distribution
+ * @return A random value from N(mean, stddev^2)
+ */
 template<typename T> T randNormal(T mean, T stddev) {
-    /**
-     * @brief Generates random value from normal distribution N(mean, stddev)
-     * 
-     * @param mean   mean of the normal distribution
-     * @param stddev standard deviation of the normal distribution
-     * @return random value from N(mean, stddev)
-     */
     std::normal_distribution<double> dist((double) mean, (double) stddev);
     return (T) dist(rng);
 }
 
+/**
+ * @brief Generates random double from [from, to).
+ * @param from The lower bound (inclusive)
+ * @param to The upper bound (exclusive)
+ * @return A random double from [from, to)
+ */
 double randDouble(double from, double to);
+
+/**
+ * @brief Generates true with probability p.
+ * @param p The probability of true
+ * @return true with probability p, false otherwise
+ */
 bool randBool(double p);
