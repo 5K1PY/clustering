fa7e30165b8cc5d7542c73a8a37e7ad42c9b261d
Daniel Skýpala <daniel@honza.info>
2025-05-06 16:38:52 +0200

    Graphs: Add dimension plotting

--- graphs.py
+++ graphs.py
@@ -25,6 +25,8 @@ SOLUTION_MARKER = {
     "Grid hashing": "^",
     "Face hashing": "v"
 }
+PLOT_DIMENSIONS = [2, 5, 10]
+PLOT_SIZES = [10000]
 
 def get_color(args: list[str]):
     if len(args) == 0:
@@ -52,7 +54,7 @@ def plot_instance(title: str, values):
             )
 
         plt.title(title)
-        plt.xlabel("Input size (n)")
+        plt.xlabel("Input size (n)" if "d=" in title else "Dimension (d)")
         plt.ylabel(val_name)
         ax.legend()
 
@@ -71,9 +73,10 @@ def plot_instance(title: str, values):
 def plot_file(filename: str):
     global d
     plot_name = "Facility location" if "fl" in filename else "Clustering"
-    plot_name += f" (d=X, z={filename[-5]})"
+    plot_name += f" ($$, z={filename[-5]})"
 
-    generated_dataset = defaultdict(list)
+    by_dimension = defaultdict(list)
+    by_size = defaultdict(list)
     with open(filename) as f:
         reader = csv.reader(f)
 
@@ -99,10 +102,16 @@ def plot_file(filename: str):
                 _, n, d, _ = inp.replace(".", "_").split("_")
                 n = int(n[1:])
                 d = int(d[1:])
-                generated_dataset[d].append((n, solution, *map(float, params)))
-    
-    for d, d_values in generated_dataset.items():
-        plot_instance(plot_name.replace("d=X", f"d={d}"), d_values)
+                by_dimension[d].append((n, solution, *map(float, params)))
+                by_size[n].append((d, solution, *map(float, params)))
+
+    for d, d_values in by_dimension.items():
+        if d in PLOT_DIMENSIONS:
+            plot_instance(plot_name.replace("$$", f"d={d}"), d_values)
+
+    for n, n_values in by_size.items():
+        if n in PLOT_SIZES:
+            plot_instance(plot_name.replace("$$", f"n={n}"), n_values)
 
 if __name__ == "__main__":
     os.makedirs(IMG_DIR, exist_ok=True)
